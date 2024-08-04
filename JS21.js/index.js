9cbdae5d80cf74641d0c049f3f9c0f18c380fa45
CodeZontes <webwithfusion@gmail.com>
2024-08-04 23:07:48 +0530

    commit

--- JS21.js/index.js
+++ JS21.js/index.js
@@ -10,9 +10,26 @@ const port = 3000;
 
 const server = http.createServer((req,res)=>{
     console.log(req.url)
+    url = req.url;
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(index);
+    if(url == '/index'){
+        res.end(index);
+    }
+    else if(url == '/About'){
+        res.end(About);
+    }
+    else if(url == '/Contact'){
+        res.end(Contact);
+    }
+    else if(url == '/Services'){
+        res.end(Services);
+    }
+    else{
+        res.statusCode= 404;
+        res.end("<h1>404 NOT FOUND</h1>")
+    }
+    
 })
 
 server.listen(port, hostname, () => {
