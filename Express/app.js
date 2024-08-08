ba125e779450e518d8cfcd113b8e3f07fa1ad1f3
CodeZontes <webwithfusion@gmail.com>
2024-08-08 22:52:06 +0530

    commmit

--- Express/app.js
+++ Express/app.js
@@ -1,54 +1,40 @@
-// Express can o the work of custom backend module that we dont have write that big code by  our small brains the express library makes oour work easy and fast it do out work of routing get and post for our web page and it is scalable 
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
 
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
 
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
 
+app.post('/', (req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    email = req.body.email
+    gender = req.body.gender
+    address = req.body.address
 
-const express = require('express'); // 1st Step Import Express module
-const path = require('path'); // Giving a string path of view directory
-const app = express();  // 2nd make the app or any name that you want to create and initialize it eith the express 
-const port = 80; // 3rd step set the port to call on the localhost 
+    let outputToWrite = `the name of the client is ${name}, ${age} years old, ${email}, ${gender}, residing at ${address}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
 
-// EXPRESS SPECIFIC STUFF HERE 
-app.use('/static',express.static('static')); // for serving static file 
-
-// PUG SPECIFIC STUFF HERE 
-app.set('view engine', 'pug') // set the Templet engine as pug
-app.get(path.join(__dirname,'views'));// set the view directory 
-
-// END POINTS 
-app.get('/',(req,res)=>{
-    const con = 'All over the best membership in the city'
-    const param = {'title': 'the best membership', 'content': con}
-    res.status(200).render("index.pug", param)
 })
 
 
-// START THE SERVER 
+// START THE SERVER
 app.listen(port, ()=>{
-   
-    console.log(`The application staarted successfully on port ${port}`);
-})
-
-// 4th using get method to handle my request by the slash(/) or any about end point  
-// And we can also add the status code in this by adding some word
-
-// app.get('/demo',(req,res)=>{
-//     res.status(200).render('demo', { title: 'Hey Harsh', message: 'Hello there! is an update on the Node js' });
-//     // res.send('Hello World');
-
-// })
-
-// app.get('/about',(req,res)=>{
-//     res.send('About Hello World ');
-// })
-
-// app.get('/this',(req,res)=>{
-//     res.status(404).send('This Page is not Found');
-// })
-
-// // This is for the post 
-// app.post('/about',(req,res)=>{
-//     res.send('About Hello World ');
-// })
-
-// 5th app.listen then apply port like whenever rq comes it reponse  
+    console.log(`The application started successfully on port ${port}`);
+});
