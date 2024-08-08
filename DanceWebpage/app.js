bcf7a58adf848b95f8895e8c7a4f88d0d56254c8
CodeZontes <webwithfusion@gmail.com>
2024-08-09 01:41:38 +0530

    commit

---
+++ DanceWebpage/app.js
@@ -0,0 +1,30 @@
+// Require call of libraries 
+const express = require('express');
+// const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Uses of the files 
+app.use('/static', express.static('static'));
+app.use(express.urlencoded());
+
+// Setting the engine 
+app.set('view engine', 'pug') // Calling the engine and on the engin of the pug library 
+app.set(path.join(__dirname, 'views')) // Setting the path of the folder where the templete is store 
+
+// Here are the end points of the get and post 
+app.get('/',(req,res)=>{
+    const params = {}
+    res.status(200).render('index.pug', params)
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
+
+
