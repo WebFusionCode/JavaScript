4c34a7426cbb39aab170a4eeddeda915bbe0240a
CodeZontes <webwithfusion@gmail.com>
2024-08-21 01:11:53 +0530

    commit

--- DanceWebpage/app.js
+++ DanceWebpage/app.js
@@ -1,34 +1,51 @@
- // Require call of libraries 
 const express = require('express');
-// const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
-// Uses of the files 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactdance');
+}
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    subject: String,
+    message: String
+});
+
+const contactp = mongoose.model('contactp', contactSchema);
+
 app.use('/static', express.static('static'));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
-// Setting the engine 
-app.set('view engine', 'pug') // Calling the engine and on the engin of the pug library 
-app.set(path.join(__dirname, 'views')) // Setting the path of the folder where the templete is store 
+// Setting the view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-// Here are the end points of the get and post 
-app.get('/',(req,res)=>{
-    const params = {}
-    res.status(200).render('home.pug', params)
-})
-app.get('/contact',(req,res)=>{
-    const params = {}
-    res.status(200).render('contact.pug', params)
-})
+// GET endpoints
+app.get('/', (req, res) => {
+    res.status(200).render('home.pug');
+});
 
+app.get('/contact', (req, res) => {
+    res.status(200).render('contact.pug');
+});
 
+// POST endpoint for handling form submission
+app.post('/contact', (req, res) => {
+    var myData = new contactp(req.body);
+    myData.save().then(() => {
+        res.send("This form has been submitted");
+    })
+    .catch(() => {
+        res.status(400).send("This form has not been submitted");
+    });
+});
 
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
-
-
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
